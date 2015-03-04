7fed6bef8c0c9f1d62dddb5ff497c6328beea1ea
Thoumou <gauvin.th@gmail.com>
2015-03-04 09:08:04 +0100

    init ID3I commit

---
+++ config_in22c.php
@@ -0,0 +1,8 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'mantis_bugtracker';
+	$g_db_username = 'mantisBT';
+	$g_db_password = 'vMJ7TaJeCFnvNAKD';
+	$g_default_language = 'french';	$g_from_email = 'postmaster@id3i.fr';
+?>
---
+++ config_inc.php
@@ -0,0 +1,9 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'mantis_bugtracker';
+	$g_db_username = 'mantisBT';
+	$g_db_password = 'vMJ7TaJeCFnvNAKD';
+	$g_default_language = 'french';
+	$g_from_email = 'noreply@id3i.fr';
+?>
